a7d2085ea14beeb294ee892451ac8fc59bbc4fe0
nahuel.palumbo@gmail.com <nahuel.palumbo@gmail.com>
2016-05-04 06:41:56 -0400

    Mumuki Export on 2016-05-04 06:41:56 -0400

---
+++ extra.pl
@@ -0,0 +1,35 @@
+% 1)
+escucha(nahue, losPiojos).
+escucha(nahue, zonaGanjah).
+escucha(nahue, sigRagga).
+escucha(fer, manuChao).
+escucha(tato, sigRagga).
+escucha(ana, charly).
+
+juega(fer, futbol).
+juega(nico, basquet).
+juega(tato, futbol).
+juega(tato, hearthstone).
+juega(ana, golf).
+
+% 2)
+hincha(fer, boca).
+hincha(nico, river).
+hincha(tato, newells).
+hincha(ana, sanLorenzo).
+
+rivales(boca, river).
+rivales(sanLorenzo, huracan).
+
+
+% 3)
+toca(zonaGanjah, lunaPark).
+toca(elBordo, lunaPark).
+toca(viejasLocas, fortin).
+
+partido(futbol, boca, river, bombonera).
+partido(basquet, velez, river, fortin).
+partido(futbol, racing, newells, cilindro).
+partido(futbol, river, boca, monumental).
+
+% 4)
